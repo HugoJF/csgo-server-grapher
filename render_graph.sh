b8bb78f3cfbbf1b6ed63608e8d51f65af0438368
Hugo <hugojellerferreira@hotmail.com>
2019-02-09 21:37:37 -0300

    added first graph version, fixed missing file prefixes

--- render_graph.sh
+++ render_graph.sh
@@ -2,22 +2,28 @@ rrdtool graph sv1.png \
 --imgformat PNG \
 --title="Server Performance" \
 --vertical-label "FPS" \
+--right-axis-label "Players" \
+--right-axis 0.1:0 \
 --start=n-61min \
 --end=n-1min \
 --color=BACK#CCCCCC \
 --color=CANVAS#FEFEFE \
 --color=SHADEB#9999CC \
 --height=250 \
---upper-limit=120 \
+--upper-limit=135 \
 --lower-limit=0 \
 --grid-dash 0:1 \
---no-gridfit \
 --watermark "Servidores de_nerdTV" \
 --slope-mode \
 DEF:sv1_fps=performance_143_202_39_221_27001.rrd:fps:AVERAGE \
-DEF:sv1_var=performance_143_202_39_221_27001.rrd:var:AVERAGE \
 DEF:sv1_players=performance_143_202_39_221_27001.rrd:players:AVERAGE \
-LINE:sv1_fps#3634c0:STACK:"Server #1"
-
-rm graphs/sv1.png
-mv player_count.png graphs/sv1.png
+CDEF:sv1_players_scaled=sv1_players,10,* \
+VDEF:sv1_fps_avg=sv1_fps,AVERAGE \
+VDEF:sv1_players_avg=sv1_players,AVERAGE \
+AREA:sv1_players_scaled#3634c066:"Players\l" \
+LINE:sv1_players_scaled#333333 \
+LINE2:sv1_fps#3634c0:"FPS\l" \
+HRULE:128#00ff00:"128 TICK\l" \
+HRULE:102#fff300:"102.8 TICK\l" \
+GPRINT:sv1_fps_avg:"AVG\: %1.1lf%sFPS" \
+GPRINT:sv1_players_avg:"PLAYER AVG\: %1.1lf%splayers\c" \
